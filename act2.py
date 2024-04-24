554b8d1e986c8f522705685a7c29e21ea7b98a1f
Macarena2023 <149072484+Macarena2023@users.noreply.github.com>
2024-04-24 20:21:23 -0300

    Create act2.py

---
+++ act2.py
@@ -0,0 +1,11 @@
+base= input("ingresar base lunar:")
+base= int(base)
+altura= input("ingresar altura lunar:")
+altura= int(altura)
+
+
+resultado= base * altura
+resultado= int(resultado)
+
+
+print("area de la superficie lunar:", resultado)
