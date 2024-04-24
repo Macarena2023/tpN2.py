5c71d5ff5e606af7227ed2f61fe60e6f8bbf4d87
Macarena2023 <149072484+Macarena2023@users.noreply.github.com>
2024-04-24 20:20:51 -0300

    Create act1.py

---
+++ act1.py
@@ -0,0 +1,8 @@
+distancia = input("ingrese la distancia entre la tierra y la luna:")
+distancia= int(distancia)
+velocidad= input("ingrese la velocidad de la nave espacial:")
+velocidad= int(velocidad)
+
+resultado = distancia + velocidad
+resultado= int(resultado)
+print("resultado:", resultado)
