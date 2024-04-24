b93dc09292d1da1261d78a190d4862e3f95db5a4
Macarena2023 <149072484+Macarena2023@users.noreply.github.com>
2024-04-24 20:25:52 -0300

    Create actMagia4.py

---
+++ actMagia4.py
@@ -0,0 +1,9 @@
+objeto_valioso= 10
+objeto_encontrado= input("definir el valor del objeto:")
+objeto_encontrado= int(objeto_encontrado)
+
+if  objeto_encontrado >= objeto_valioso:
+  print("has encontrado un tesoro oculto")
+else:
+    print("has encontrado un adorno decorativo")
+
